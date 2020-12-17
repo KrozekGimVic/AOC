1df9ba7032f98e6856989fcc03ec5d2453005904
Jure Slak <jure.slak@gmail.com>
2020-12-17 18:13:32 +0100

    Part 2.

--- jureslak/src/day16.rs
+++ jureslak/src/day16.rs
@@ -1,5 +1,5 @@
 use crate::common::Part;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::ops::Range;
 
 struct Rules {
@@ -16,7 +16,7 @@ fn parse_rules(s: &String) -> (String, Vec<Range<i32>>) {
     (parts[0].to_owned(), ranges)
 }
 
-fn compute_error_rate(r: &Rules, tickets: &Vec<Vec<i32>>) -> i32 {
+fn valid_ranges(r: &Rules) -> Vec<bool> {
     let mut valid = vec![false; 1000];
     for rules in r.rules.values() {
         for span in rules {
@@ -25,7 +25,37 @@ fn compute_error_rate(r: &Rules, tickets: &Vec<Vec<i32>>) -> i32 {
             }
         }
     }
+    valid
+}
+
+fn compute_error_rate(r: &Rules, tickets: &Vec<Vec<i32>>) -> i32 {
+    let valid = valid_ranges(r);
     tickets.iter().filter_map(|t| t.iter().find(|&v| !valid[*v as usize])).sum()
+}
+
+fn guess_fields(r: &Rules, tickets: &Vec<Vec<i32>>) -> Vec<String> {
+    let valid = valid_ranges(r);
+    let tickets : Vec<&Vec<i32>> = tickets.iter().filter(|&t| !t.iter().any(|v| !valid[*v as usize])).collect();
+    let mut possible = vec![HashSet::new(); tickets[0].len()];
+    for i in 0..tickets[0].len() {
+        for (field, rules) in r.rules.iter() {
+            if tickets.iter().all(|t| rules.iter().any(|r| r.contains(&t[i]))) {
+                possible[i].insert(field);
+            }
+        }
+    }
+
+    let mut fields = vec![String::new(); tickets[0].len()];
+    loop {
+        let determined = possible.iter().enumerate().find(|&(_i, s)| s.len() == 1);
+        match determined {
+            None => return fields,
+            Some((i, field)) => {
+                fields[i] = (*field.iter().next().unwrap()).clone();
+                possible.iter_mut().for_each(|h| { h.remove(&fields[i]); });
+            },
+        }
+    }
 }
 
 pub fn solve(data : &Vec<String>, part : Part) {
@@ -40,6 +70,16 @@ pub fn solve(data : &Vec<String>, part : Part) {
 
     match part {
         Part::First => println!("{}", compute_error_rate(&rules, &nearby)),
-        Part::Second => println!("{}", 1),
+
+        Part::Second => {
+            let fields= guess_fields(&rules, &nearby);
+            let mut r = 1i64;
+            for (i, c) in fields.iter().enumerate() {
+                if c.starts_with("departure") {
+                    r *= my_ticket[i] as i64;
+                }
+            }
+            println!("{}", r);
+        },
     }
 }
