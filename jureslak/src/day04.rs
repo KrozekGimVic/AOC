eee4b6378fb0886cb7a7abc4a3aa71af32ee22c2
Jure Slak <jure.slak@gmail.com>
2023-12-06 22:17:27 +0100

    day 4

--- jureslak/src/day04.rs
+++ jureslak/src/day04.rs
@@ -1,97 +1,54 @@
 use crate::common::Part;
-use std::str::FromStr;
-use std::error;
-use std::fmt;
 
-struct Draw {
-    red: i32,
-    green: i32,
-    blue: i32,
+struct Card {
+    win: Vec<i32>,
+    have: Vec<i32>,
 }
 
-type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
-
-#[derive(Debug)]
-struct UnknownColor;
-impl fmt::Display for UnknownColor {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Number out of valid range or otherwise invalid data...")
+impl Card {
+    fn num_winning(&self) -> i32 {
+        self.have.iter().map(|n| if self.win.contains(n) { 1 } else { 0 } ).sum()
     }
-}
-impl error::Error for UnknownColor {}
-
-impl FromStr for Draw {
-    type Err = Box<dyn error::Error>;
-    fn from_str(s: &str) -> Result<Self> {
-        let splitter = s.split(", ");
-        let mut draw = Draw { red: 0, green: 0, blue: 0};
-        for color in splitter {
-            let mut draw_splitter = color.splitn(2, ' ');
-            let num : i32 = draw_splitter.next().unwrap().parse()?;
-            let col = draw_splitter.next().unwrap();
-
-            if col == "red" {
-                draw.red = num;
-            } else if col == "blue" {
-                draw.blue = num;
-            } else if col == "green" {
-                draw.green = num;
-            } else {
-                return Err(UnknownColor{})?
-            }
+    fn points(&self) -> i32 {
+        let c = self.num_winning();
+        if c == 0 {
+            0
+        } else {
+            1 << (c - 1)
         }
-        Ok(draw)
     }
 }
 
-struct Game {
-    id: i32,
-    draws: Vec<Draw>
-}
-
-impl FromStr for Game {
-    type Err = Box<dyn error::Error>;
-    fn from_str(s: &str) -> Result<Self> {
-        let mut splitter = s.splitn(2, ": ");
-        let game = splitter.next().unwrap();
-
-        let mut game_splitter = game.splitn(2, ' ');
-        game_splitter.next();
-        let game_id : i32 = game_splitter.next().unwrap().parse()?;
-
-        let draws = splitter.next().unwrap();
-
-        Ok(Game {
-            id: game_id,
-            draws: draws.split("; ").map(|s| s.parse().unwrap()).collect()
-        })
+fn process_cards(cards: &Vec<Card>) -> i32 {
+    let mut card_count = vec![1; cards.len()];
+    for (i, c) in cards.iter().enumerate() {
+        let w = c.num_winning();
+        for j in 0..w {
+            card_count[i + j as usize + 1] += card_count[i]
+        }
     }
+    card_count.iter().sum()
 }
-
-fn is_ok(draw: &Draw) -> bool {
-    draw.red <= 12 && draw.green <= 13 && draw.blue <= 14
-}
-
 
 pub fn solve(data : &Vec<String>, part : Part) {
-    let games : Vec<Game> = data.iter().map(|s| s.parse().expect("parsing failed")).collect();
+    let mut cards = vec![];
+    for s in data {
+        let c = s.find(':').unwrap();
+        let mut splitter = s[c+2..].split(" | ");
+        let p1 = splitter.next().unwrap();
+        let p2 = splitter.next().unwrap();
+
+        let win : Vec<i32> = p1.split_whitespace().map(|s| s.parse().unwrap()).collect();
+        let have : Vec<i32> = p2.split_whitespace().map(|s| s.parse().unwrap()).collect();
+        cards.push(Card{ win, have });
+    }
 
     match part {
         Part::First => {
-            println!("{}", games.iter().filter(|g| g.draws.iter().all(is_ok)).map(|g| g.id).sum::<i32>());
+            println!("{}", cards.iter().map(|c| c.points()).sum::<i32>());
         }
         Part::Second => {
-            let power_sum : i32 = games.iter().map(|game| {
-                let mut d = Draw { red: 0, green: 0, blue: 0};
-                for draw in game.draws.iter() {
-                    d.red = d.red.max(draw.red);
-                    d.green = d.green.max(draw.green);
-                    d.blue = d.blue.max(draw.blue);
-                }
-                d.red*d.green*d.blue
-            }).sum();
-
-            println!("{}", power_sum);
+            println!("{}", process_cards(&cards));
 
         }
     }
