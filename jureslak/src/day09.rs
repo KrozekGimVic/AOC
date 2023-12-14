84ef9e0f22f08a542b4d5964e17f027c5745d90a
Jure Slak <jure.slak@gmail.com>
2023-12-14 14:21:25 +0100

    9

--- jureslak/src/day09.rs
+++ jureslak/src/day09.rs
@@ -1,31 +1,36 @@
-use std::iter::zip;
 use crate::common::Part;
 
-fn num_wins(max_time: i64, target_dist: i64) -> i32 {
-    let mut num_wins = 0;
-    for t in 1..max_time {
-        // hold for t, get speed t, run for max_time-t seconds
-        let speed = t;
-        let run_time = max_time-t;
-        let dist = run_time*speed;
-        if dist > target_dist {
-            num_wins += 1;
+fn extrapolate(data: &Vec<i32>) -> i32 {
+    let n = data.len();
+    let mut m : Vec<Vec<i32>> = vec![vec![0; n+1]; n+1];
+    for j in 0..n {
+        m[0][j] = data[j];
+    }
+    for i in 1..n {
+        for j in 0..(n-i) {
+            m[i][j] = m[i-1][j+1] - m[i-1][j];
         }
     }
-    num_wins
+    for i in 1..n {
+        m[n-i-1][i+1] = m[n-i][i] + m[n-i-1][i]
+    }
+    m[0][n]
 }
 
 pub fn solve(data : &Vec<String>, part : Part) {
+    let nums : Vec<Vec<i32>> = data.iter().map(|s| s.split_whitespace().map(|x| x.parse().unwrap()).collect()).collect();
     match part {
         Part::First => {
-            let times : Vec<i32> = data[0].split_whitespace().skip(1).map(|s| s.parse().unwrap()).collect();
-            let dist : Vec<i32> = data[1].split_whitespace().skip(1).map(|s| s.parse().unwrap()).collect();
-            println!("{}", zip(times.iter(), dist.iter()).map(|(&a, &b)| num_wins(a as i64, b as i64)).product::<i32>());
+            let r : i32 = nums.iter().map(extrapolate).sum();
+            println!("{}", r);
         }
         Part::Second => {
-            let time : i64 = data[0].split_whitespace().skip(1).collect::<Vec<_>>().join("").parse().unwrap();
-            let dist : i64 = data[1].split_whitespace().skip(1).collect::<Vec<_>>().join("").parse().unwrap();
-            println!("{}", num_wins(time, dist));
+            let r : i32 = nums.iter().map(|r| {
+                let mut v = r.clone();
+                v.reverse();
+                extrapolate(&v)
+            }).sum();
+            println!("{}", r);
 
         }
     }
