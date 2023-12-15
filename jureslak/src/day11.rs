e0337159161f66d6032c1a78d94088f8bb8e4af3
Jure Slak <jure.slak@gmail.com>
2023-12-15 08:32:06 +0100

    11

--- jureslak/src/day11.rs
+++ jureslak/src/day11.rs
@@ -1,32 +1,65 @@
-use std::iter::zip;
 use crate::common::Part;
 
-fn num_wins(max_time: i64, target_dist: i64) -> i32 {
-    let mut num_wins = 0;
-    for t in 1..max_time {
-        // hold for t, get speed t, run for max_time-t seconds
-        let speed = t;
-        let run_time = max_time-t;
-        let dist = run_time*speed;
-        if dist > target_dist {
-            num_wins += 1;
+fn get_coords(map: &Vec<Vec<u8>>, f: isize) -> Vec<isize> {
+    let mut y = vec![];
+    let mut c = 0;
+    let h = map.len();
+    let w = map[0].len();
+    for i in 0..h {
+        if map[i].iter().all(|c| *c == b'.') {
+            c += f;
+        } else {
+            for j in 0..w {
+                if map[i][j] == b'#' {
+                    y.push(c);
+                }
+            }
+            c += 1;
         }
     }
-    num_wins
+    y
+}
+
+fn rot(map: &Vec<Vec<u8>>) -> Vec<Vec<u8>> {
+    let h = map.len();
+    let w = map[0].len();
+    let mut r = vec![vec![b'.'; h]; w];
+    for i in 0..h {
+        for j in 0..w {
+            r[j][h-i-1] = map[i][j];
+        }
+    }
+    r
+}
+
+fn sumall(coor: &Vec<isize>) -> isize {
+    let mut total = 0;
+    let mut s = coor[0];
+    for i in 1..coor.len() {
+        total += (i as isize) * (coor.len()-i) as isize * (coor[i] - s);
+        s = coor[i];
+    }
+    total
 }
 
 pub fn solve(data : &Vec<String>, part : Part) {
+    let map : Vec<Vec<u8>> = data.iter().map(|s| s.as_bytes().to_vec()).collect();
+    const FF : isize = 1000000;
+    let x = get_coords(&map, 2);
+    let x2 = get_coords(&map, FF);
+    let rotmap = rot(&map);
+    let y = get_coords(&rotmap, 2);
+    let y2 = get_coords(&rotmap, FF);
+
+    assert_eq!(sumall(&vec![1,2]), 1);
+    assert_eq!(sumall(&vec![1,2,3]), 4);
+
     match part {
         Part::First => {
-            let times : Vec<i32> = data[0].split_whitespace().skip(1).map(|s| s.parse().unwrap()).collect();
-            let dist : Vec<i32> = data[1].split_whitespace().skip(1).map(|s| s.parse().unwrap()).collect();
-            println!("{}", zip(times.iter(), dist.iter()).map(|(&a, &b)| num_wins(a as i64, b as i64)).product::<i32>());
+            println!("{}", sumall(&x) + sumall(&y));
         }
         Part::Second => {
-            let time : i64 = data[0].split_whitespace().skip(1).collect::<Vec<_>>().join("").parse().unwrap();
-            let dist : i64 = data[1].split_whitespace().skip(1).collect::<Vec<_>>().join("").parse().unwrap();
-            println!("{}", num_wins(time, dist));
-
+            println!("{}", sumall(&x2) + sumall(&y2));
         }
     }
 }
